4459e56676c24cb8bb3267170cddf839169eff93
jamesyim <theladjames@gmail.com>
2025-01-27 16:11:29 +0000

    Refactor Todo List application with improved theme toggle functionality, enhanced HTML structure, and updated styles

--- src/client/index.js
+++ src/client/index.js
@@ -1,25 +1,37 @@
 import "./styles.css";
 
-const themeBtn = document.getElementById('moon-icon');
-const todoInput = document.getElementById("create-todo-input");
+document.addEventListener('DOMContentLoaded', () => {
 
-function toggleTheme() {
-    const DARKCOLOR = 'rgb(14, 10, 15)';
-    const LIGHTCOLOR = 'rgb(255, 255, 255)';
-    if (document.body.classList.contains('light')) {
-        document.body.style.backgroundColor = LIGHTCOLOR;
-        document.body.style.color = DARKCOLOR;
-        themeBtn.innerHTML = `<i class="fa-solid fa-moon fa-2x"></i>`;
-    } else if (document.body.classList.contains('dark')) {
-        document.body.style.backgroundColor = DARKCOLOR;
-        document.body.style.color = LIGHTCOLOR;
-        themeBtn.innerHTML = `<i class="fa-solid fa-sun fa-2x"></i>`;
+    const themeBtn = document.getElementById('moon-icon');
+    const todoInput = document.getElementById('create-todo-input');
+    const headerElement = document.getElementById('header');
+    const inputTodoContainer = document.querySelector('.input-todo');
+
+    function toggleTheme() {
+        const DARKCOLOR = 'rgb(14, 10, 15)';
+        const LIGHTCOLOR = 'rgb(255, 255, 255)';
+        if (document.body.classList.contains('light')) {
+            document.body.style.backgroundColor = LIGHTCOLOR;
+            document.body.style.color = DARKCOLOR;
+            themeBtn.innerHTML = `<i class="fa-solid fa-moon fa-2x"></i>`;
+            headerElement.style.backgroundImage = "url('/assets/sticky-unsplash.webp)";
+            headerElement.style.color = DARKCOLOR;
+        } else if (document.body.classList.contains('dark')) {
+            document.body.style.backgroundColor = DARKCOLOR;
+            document.body.style.color = LIGHTCOLOR;
+            themeBtn.innerHTML = `<i class="fa-solid fa-sun fa-2x"></i>`;
+            headerElement.style.backgroundImage = "url('/assets/sticky-3-unsplash.webp')";
+            inputTodoContainer.style.backgroundColor = 'rgba(14, 10, 15, 0.25)';
+            inputTodoContainer.style.color = LIGHTCOLOR;
+            headerElement.style.color = LIGHTCOLOR;
+        }
     }
-}
 
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light');
-    document.body.classList.toggle('dark');
+    themeBtn.addEventListener('click', () => {
+        document.body.classList.toggle('light');
+        document.body.classList.toggle('dark');
+        toggleTheme();
+    });
+
     toggleTheme();
 });
-
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const htmlPages = [
     "index.html"
@@ -29,7 +31,7 @@ module.exports = (_env, argv) => {
                     test: /\.(png|svg|jpg|jpeg|gif)$/i,
                     type: 'asset/resource',
                     generator: {
-                        filename: 'assets/[name][ext]',
+                        filename: 'assets/[name][hash][ext]',
                     },
                 },
                 {
@@ -43,6 +45,23 @@ module.exports = (_env, argv) => {
                     },
                 },
             ]
+        },
+        optimization: {
+            minimizer: [
+                '...',
+                new ImageMinimizerPlugin({
+                    minimizer: {
+                        implementation: ImageMinimizerPlugin.imageminGenerate,
+                        options: {
+                            plugins: [
+                                ['mozjpeg', { quality: 60 }],
+                                ['pngquant', { quality: [0.5, 0.65] }],
+                                ['webp', { quality: 60 }],
+                            ],
+                        },
+                    },
+                }),
+            ],
         },
         resolve: {
             extensions: ['.js', '.css'],
@@ -77,6 +96,14 @@ module.exports = (_env, argv) => {
                 }),
             isProduction && new MiniCssExtractPlugin({
                 filename: "[name].[contenthash].css",
+            }),
+            new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets',
+                    to: 'assets'
+                },
+            ],
             }),
         ].filter(Boolean),
     };
